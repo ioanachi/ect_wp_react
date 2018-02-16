85fab8693a69ff2ca93178b96cb5693be2e05ea2
turcuciprian <turcuciprian1@gmail.com>
2018-02-17 00:26:03 +0200

    fixed Y2S

--- src/js/timerStyles/pack1/plainString.js
+++ src/js/timerStyles/pack1/plainString.js
@@ -6,10 +6,18 @@ export default class PlainString extends React.Component {
   }
   render() {
     var finalResult = [];
-    for(var key in this.props.numbers){
-      if(key!='Styles'){
-        var tempItem = (<div className="spanDiv"><span style={this.props.numbers.Styles}>{this.props.numbers[key]}</span> <span style={this.props.cTxt.Styles}>{this.props.cTxt[key]}</span> /&nbsp;</div>);
-        finalResult.push(tempItem);
+    var isLast = true;
+    for (var key in this.props.numbers) {
+      if (key != 'Styles') {
+        if (isLast) {
+          if (this.props.numbers[key] != 0) {
+            isLast = false;
+          }else{
+            continue;
+          }
+        }
+          var tempItem = (<div className="spanDiv"><span style={this.props.numbers.Styles}>{this.props.numbers[key]}</span> <span style={this.props.cTxt.Styles}>{this.props.cTxt[key]}</span>&nbsp;</div>);
+          finalResult.push(tempItem);
       }
     }
     return (
