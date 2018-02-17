88b9670360dd3134783b14c6334a0bc24c46eb20
ioanachi <ioana_chichi@outlook.com>
2018-02-17 18:23:39 +0200

    deleted dayPicker

--- src/app.js
+++ src/app.js
@@ -1,9 +1,7 @@
 import "../index.html";
 import 'react';
 import 'react-dom';
-import 'react-day-picker';
-import 'react-day-picker/moment';
-import 'react-day-picker/lib/style.css';
+
 import  'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
