24a4c44e2968c3a2de729b5d46f489ccd888f760
ioanachi <ioana_chichi@outlook.com>
2018-02-24 02:43:11 +0200

    excluded layouts

---
+++ src/js/allTimers.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+
+
+class AllTimers extends React.Component {
+    constructor(props) {
+        super(props);
+        
+
+    }
+
+
+    render() {
+
+        return
+    }
+}
+ReactDOM.render(
+    <AllTimers  />,
+    document.getElementById("allTimers")
+);
+
