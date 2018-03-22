fd25a36d45742119ee276bd75caa1f74b41efadf
turcuciprian <turcuciprian1@gmail.com>
2018-03-23 00:15:54 +0200

    fixed settimeout bug

--- src/js/Components/livePreview.js
+++ src/js/Components/livePreview.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import mathCountDown from './mathCountDown';
-import {
-    HorizontalTimer,
-    VerticalTimer,
-    CalendarTimer,
-    SeparateTimer
-} from '../layouts/pack1';
+import {HorizontalTimer, VerticalTimer, CalendarTimer, SeparateTimer} from '../layouts/pack1';
 export class LivePreview extends React.Component {
     constructor(props) {
         super(props);
@@ -15,29 +10,25 @@ export class LivePreview extends React.Component {
             layoutType: this.props.pAllData.layoutType
         };
         const tempClass = window[this.state.layoutType + '2'];
-        this.handleScroll = this.handleScroll.bind(this);
+        this.handleScroll = this
+            .handleScroll
+            .bind(this);
 
     };
 
     dinamicComponent() {
-        var tempTimeout = this.state.timeout;
+        let tempTimeout = this.state.timeout;
         if (!tempTimeout[0]) {
-            tempTimeout.push(setTimeout(() => {
+            tempTimeout.push(tempTimeout.push(setTimeout(() => {
 
-                this.setState({
-                    timeout: []
-                });
-                this.setState({
-                    layoutType: this.props.pAllData.layoutType
-                }); //new layout type
-            }, 1000));
+                this.setState({timeout: []});
+                this.setState({layoutType: this.props.pAllData.layoutType}); //new layout type
+            }, 1000)));
         }
         var tempTimeout = this.state.timeout;
         if (!tempTimeout[0]) {
             tempTimeout.push(setTimeout(() => {
-                this.setState({
-                    timeout: []
-                });
+                this.setState({timeout: []});
             }, 1000));
         }
         var dataProps = {
@@ -105,32 +96,32 @@ export class LivePreview extends React.Component {
             divStyle = {
                 fontSize: dataProps.fontSize + 'px',
                 color: dataProps.color,
-                fontWeight: (dataProps.isBold == true ?
-                    'bold' :
-                    'normal')
+                fontWeight: (dataProps.isBold == true
+                    ? 'bold'
+                    : 'normal')
             }
             divStyleTxt = {
                 fontSize: dataProps.fontSizeTxt + 'px',
                 color: dataProps.colorTxt,
-                fontWeight: (dataProps.isBoldTxt == true ?
-                    'bold' :
-                    'normal')
+                fontWeight: (dataProps.isBoldTxt == true
+                    ? 'bold'
+                    : 'normal')
             };
         } else {
 
             divStyle = {
                 fontSize: this.props.pAllData.fontSize + 'px',
                 color: this.props.pAllData.color,
-                fontWeight: (this.props.pAllData.isBold == true ?
-                    'bold' :
-                    'normal')
+                fontWeight: (this.props.pAllData.isBold == true
+                    ? 'bold'
+                    : 'normal')
             }
             divStyleTxt = {
                 fontSize: this.props.pAllData.fontSizeTxt + 'px',
                 color: this.props.pAllData.colorTxt,
-                fontWeight: (this.props.pAllData.isBoldTxt == true ?
-                    'bold' :
-                    'normal')
+                fontWeight: (this.props.pAllData.isBoldTxt == true
+                    ? 'bold'
+                    : 'normal')
             }
         }
         // all the available layouts
@@ -166,7 +157,6 @@ export class LivePreview extends React.Component {
             }
         }
 
-
         const timerCustomTxt = {
             Years: dataProps.yearsTxt,
             Months: dataProps.monthsTxt,
@@ -180,21 +170,29 @@ export class LivePreview extends React.Component {
         }
         const DynamicComponentName = components[this.props.pAllData.layoutType];
 
-        return (<DynamicComponentName className="floatingPreview" numbers={
-            timerNumbers
-        }
-            customTxt={
-                timerCustomTxt
-            }
-        />);
+        return (<DynamicComponentName
+            className="floatingPreview"
+            numbers={timerNumbers}
+            customTxt={timerCustomTxt}/>);
     }
     handleScroll() {
-        var previewOffset = ReactDOM.findDOMNode(this.refs['boxPreview']).getBoundingClientRect();
+        var previewOffset = ReactDOM
+            .findDOMNode(this.refs['boxPreview'])
+            .getBoundingClientRect();
         if (previewOffset.y <= 1) {
-            ReactDOM.findDOMNode(this.refs['containerPreview']).style.position = "fixed";
-            ReactDOM.findDOMNode(this.refs['containerPreview']).style.top = "0px";
+            ReactDOM
+                .findDOMNode(this.refs['containerPreview'])
+                .style
+                .position = "fixed";
+            ReactDOM
+                .findDOMNode(this.refs['containerPreview'])
+                .style
+                .top = "0px";
         } else {
-            ReactDOM.findDOMNode(this.refs['containerPreview']).style.position = "relative";
+            ReactDOM
+                .findDOMNode(this.refs['containerPreview'])
+                .style
+                .position = "relative";
         }
     }
     componentDidMount() {
@@ -202,15 +200,12 @@ export class LivePreview extends React.Component {
     };
     render() {
 
-        return (
+        return ( <div className = "previewBox" ref = "boxPreview" > <div
+            className={'containerPreview ' + this.props.livePreviewOnly}
+            ref="containerPreview">
+            {this.dinamicComponent()}
 
-            < div className="previewBox" ref="boxPreview">
-              
-                    <div className={'containerPreview ' + this.props.livePreviewOnly} ref="containerPreview">
-                        {this.dinamicComponent()}
-                
-                </div>
-            </div >
+        </div> </div>
         );
     }
 }
